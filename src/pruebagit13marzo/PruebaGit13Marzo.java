c33479f76753eeb480c60e321183b85fc472e76c
ebalseraq01 <ennybalsera@hotmail.com>
2018-03-13 09:15:41 +0100

    Commit2.- Actualizacion 2- Encarna

--- src/pruebagit13marzo/PruebaGit13Marzo.java
+++ src/pruebagit13marzo/PruebaGit13Marzo.java
@@ -17,7 +17,9 @@ public class PruebaGit13Marzo {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.print("HOla que tal??");
+        System.out.print("HOla que tal?? Soy pepito");
+        
+        
     }
     
 }
